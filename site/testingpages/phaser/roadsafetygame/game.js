963b993521bc2a389058b95558116e22475a2305
MrSp33dy123 <roozeno@student.lincoln.school.nz>
2016-05-26 13:45:30 +1200

    Update game.js with AJAX code

--- site/testingpages/phaser/roadsafetygame/game.js
+++ site/testingpages/phaser/roadsafetygame/game.js
@@ -1,32 +1,39 @@
 "use strict";
-var mapData = [];
+var locIteration = 0;
+var mapData = [
+    [-43.640022, 172.486058, false, 'There are multiple cars approaching on both sides of the road. You must wait for a large enough gap in the traffic to safely cross.\nIt is not a good idea to cross here as there is a designated pedestrian crossing only a few metres down the road. Cross there.'],
+    [-43.634008, 172.486040, true, "It's scarcer out here than in a Mad Max movie. Although there is no pedestrian crossing, there are no cars as far as the eye can see and this road has no blind spots."]
+];
 
 function initGame() {
-    initMap();
-    getLocationInfo();
-    
+    var map = new google.maps.Map(document.getElementById('map'), {
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
+        scrollwheel: true,
+        zoom: 8,
+        streetViewControl: false
+    });
+    getMapData();
     console.log(mapData);
+    
+    $('#nextLoc').click(function(){
+        if (locIteration < mapData.length) {
+            map.setCenter({lat: mapData[locIteration][0], lng: mapData[locIteration][1]});
+            locIteration++;
+        } else {
+            alert('Map complete!');
+        }
+    });
 }
 
-function getLocationInfo() {
-    $.getJSON("ajax/servelocationinfo.php", {'mapID':''}, function(result, status){
+function getMapData(answerInfo) {
+    if (answerInfo == null) {answerInfo = []};
+
+    $.getJSON("testingpages/phaser/roadsafetygame/ajax/servegamedata.php", {'answer':answer, 'map':map, 'round':round}, function(result, status){
         if (status == "success") {
-            mapData = result;
+            return result;
         } else if (status != "notmodified") {
             alert("AJAX Error! Contact website adminstrator.");
         }
     }); 
-}
+};
 
-function initMap() {
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    // Set mapTypeId to google.maps.MapTypeId.SATELLITE in order
-    // to activate satellite imagery.
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    scrollwheel: true,
-    zoom: 8,
-    streetViewControl: true
-  });
-}
