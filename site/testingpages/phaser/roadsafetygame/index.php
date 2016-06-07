91635e5df59e89bc5e7927f5fd4311e1ab8f1caf
MrSp33dy123 <mrbondusa5@gmail.com>
2016-06-08 01:31:42 +1200

    Small Update
    Small update to styling for 'select map' screen.

--- site/testingpages/phaser/roadsafetygame/index.php
+++ site/testingpages/phaser/roadsafetygame/index.php
@@ -66,7 +66,7 @@
                         </a>
                         <div id="selectMap">
                             <div>
-                                <img alt="" src="">
+                                <img alt="Christchurch, New Zealand" src="testingpages/phaser/roadsafetygame/media/default.jpg">
                                 <p class="title">Christchurch, New Zealand</p>
                                 <p class="description">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                             </div>
